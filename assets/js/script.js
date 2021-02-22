e92356b46004fb2fef189d54c442e31dcd49aa33
01abhishek10 <itsabhihere@yahoo.com>
2021-02-22 09:33:48 +0530

    added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ function load() {
     var element = document.getElementById('loader');
     element.style.display = "none";
 }
+function changeATag() {
+    var element = document.getElementById('icon-hanging');
+    if (window.scroll > 300) {
+    element.style.color = "black";
+    }
+    
+}
